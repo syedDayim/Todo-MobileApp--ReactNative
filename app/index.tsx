348177bf74382f38ef549de57e6b11a7811418bb
syedDayim <dayim1277@gmail.com>
2024-07-02 15:40:34 +0530

    Created List

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,45 @@
-import { Text, View } from "react-native";
+import React, { useState } from "react";
+import { FlatList, Text, View, StyleSheet } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
+  const items = [
+    { task: "Learn React Native", id: "1" },
+    { task: "Learn Routing", id: "2" },
+    { task: "Make Projects", id: "3" },
+  ];
+
+  const [tasks, setTasks] = useState(items);
+
   return (
-    <View>
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <SafeAreaView>
+      {/* Text Input will go here */}
+      {/* List of Todo will go here */}
+
+      <FlatList
+        style={{ marginTop: 60 }}
+        data={tasks}
+        keyExtractor={(item) => item.id} // Convert id to string
+        renderItem={({ item }) => (
+          <View style={styles.listItemContainer}>
+            <Text style={styles.listItemText}>{item.task}</Text>
+          </View>
+        )}
+      />
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  listItemContainer: {
+    padding: 10,
+    borderWidth: 1,
+    marginVertical: 5,
+    marginHorizontal: 20,
+    borderRadius: 6,
+  },
+  listItemText: {
+    fontWeight: "700",
+    fontSize: 15,
+  },
+});
